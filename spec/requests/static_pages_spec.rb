95dd1ca41ecb792fa1edb65ed3298e4a39ef28ac
Adam Jackman <adam@acjackman.com>
2012-07-28 00:31:10 -0700

    Refactor Static_Pages Spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,65 +2,33 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+  
   describe "Home page" do
-
-    it "should have the content 'Quote'" do
-      visit root_path
-      page.should have_content('Quote')
-    end
+    before { visit root_path }
     
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-                        text: "Adam's Quotes")
-    end
-    
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', text: '| Home')
-    end
+    it { should have_selector('h1', text: 'Quote') }
+    it { should have_selector('title', text: full_title('')) }
   end
   
   describe "Help page" do
-    
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                    text: "Adam's Quotes | Help")
-    end
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
-  
+
   describe "About page" do
-    
-    it "should have the content 'About this site'" do
-      visit about_path
-      page.should have_content('About this site')
-    end
-    
-    it "should have the title 'About'" do
-      visit about_path
-      page.should have_selector('title',
-                    text: "Adam's Quotes | About")
-    end
+    before { visit about_path }
+
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('About')) }
   end
-  
+
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                    text: "Adam's Quotes | Contact")
-    end
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
-  
 end
