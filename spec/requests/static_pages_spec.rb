e77ac2730fcd70c10d494d41cec6a6b4e27b517b
Adam Jackman <adam@acjackman.com>
2012-07-27 23:45:34 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,11 @@ describe "Static pages" do
       page.should have_selector('title',
                     text: "Adam's Quotes | Home")
     end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '| Home')
+    end
   end
   
   describe "Help page" do
