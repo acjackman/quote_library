ae9664761aa4a33c0f60ca1ed7c1f8b5632a4141
Adam Jackman <adam@acjackman.com>
2012-07-28 01:35:55 -0700

    Make User Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 QuoteLibrary::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
   
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,8 +3,17 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 
   describe "signup page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
