1dc0e0097586cd08027dfbab48ec787204256ed0
Adam Jackman <adam@acjackman.com>
2012-07-28 11:09:07 -0700

    Make signin and signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+      flash[:success] = "Welcome to Adam's Quotes!"
       redirect_to @user
     else
       render 'new'
